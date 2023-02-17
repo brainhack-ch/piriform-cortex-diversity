dcdbdb14857d9f3cb967007fd67fedc3b2c249b5
Alex Cionca <cionkito@gmail.com>
2023-02-17 18:05:34 +0100

    Updated data_loader.py with progress bar and verbose options

--- code/data_loaders.py
+++ code/data_loaders.py
@@ -6,7 +6,9 @@ import numpy as np
 import os
 import os.path as op
 
-def genenerate_dendrite_df(path_to_xls):
+import ipywidgets
+
+def genenerate_dendrite_df(path_to_xls, verb = True):
 
     # Load neuron excel file
     neuron_excel = pd.ExcelFile(path_to_xls)
@@ -22,8 +24,9 @@ def genenerate_dendrite_df(path_to_xls):
     # Drop last empty row
     dendrite_df = dendrite_df.dropna(axis = 0, subset = ['ID'])
 
-    # Convert ID to int (from scientific)
-    dendrite_df.loc[:, 'ID'] = dendrite_df.ID.astype(int)
+    # Convert ID to int (from scientific) - Warning
+    dendrite_df['ID'] = dendrite_df.ID.astype(int).values
+    #dendrite_df.loc[:, 'ID'] = dendrite_df.loc[:, 'ID'].astype(int).values
 
     # Iterate over features
     for feat in dendrite_features[1:]:
@@ -37,8 +40,9 @@ def genenerate_dendrite_df(path_to_xls):
 
         # Add new feature to existing dataframe
         dendrite_df = dendrite_df.merge(feat_df, on = 'ID', how = 'outer')
-
-    print(f'Successfully converted data from {dendrite_df.shape[0]} dendrites ({dendrite_df.shape[1]} features found).')
+    
+    if verb:
+        print(f'Successfully converted data from {dendrite_df.shape[0]} dendrites ({dendrite_df.shape[1]} features found).')
 
     return dendrite_df
 
@@ -65,7 +69,7 @@ def get_neuron_specific_features(path_to_xls, features_list = ['Filament Boundin
 
     return neuron_spec_features
 
-def excel_to_neuron(path_to_xls):
+def excel_to_neuron(path_to_xls, **kwargs):
 
     # Parse neuron id from path to excel file ('.' split removes file extension and ' ' removes potential ' - Copy')
     neuron_id = op.basename(path_to_xls).split('.')[0].split(' ')[0]
@@ -78,7 +82,7 @@ def excel_to_neuron(path_to_xls):
     new_neuron.add_features(neuron_features)
  
     # Gather dendrite data in the form of a DataFrame
-    dendrite_df = genenerate_dendrite_df(path_to_xls = path_to_xls)
+    dendrite_df = genenerate_dendrite_df(path_to_xls = path_to_xls, **kwargs)
     new_neuron.add_dendrites(dendrite_df)
 
     return new_neuron
@@ -88,23 +92,32 @@ def get_neuron_files_in_dir(dir_path):
     files = os.listdir(xls_path)
     return [op.join(xls_path,f) for f in files if f.endswith("xls")]
 
-def get_neuron_matrix(dir_path):
+def get_neuron_matrix(dir_path, **kwargs):
     path = get_neuron_files_in_dir(dir_path)
 
-    first_neuron = excel_to_neuron(path[0])
+    first_neuron = excel_to_neuron(path[0], **kwargs)
     features_names = first_neuron.get_feature_dict().keys()
 
     n_cells = len(path)
     n_features = len(features_names)
 
     neuron_df = pd.DataFrame(np.zeros((n_cells, n_features)), columns = features_names)
+    
+    progressbar = ipywidgets.IntProgress(value=1, min=1, max=n_cells,
+                                             description='Loading data:', bar_style='',
+                                             style={'bar_color': '#AAAAAA'},
+                                             orientation='horizontal')
+    display(progressbar)
 
     neuron_ids = []
     for i, neuron_path in enumerate(path):
-        myneuron = excel_to_neuron(neuron_path)
+        myneuron = excel_to_neuron(neuron_path, **kwargs)
         neuron_ids.append(myneuron.id)
         features = myneuron.get_feature_dict()
         neuron_df.loc[i]= features
+        
+        progressbar.value += 1
+        
     neuron_df['id'] = neuron_ids
     neuron_df.set_index('id', inplace=True)
     return neuron_df
